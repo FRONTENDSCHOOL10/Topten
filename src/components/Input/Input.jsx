932f38d1fa3d3a4aa976a5e656da1e85f161d1de
kwonboryong <kwonboryong@naver.com>
2024-09-03 17:33:01 +0900

    update: 이메일 인증 버튼 조건부 UI

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Input.module.scss';
+
 function Input(props) {
   return (
     <div className={styles.inputComponent}>
@@ -8,7 +9,14 @@ function Input(props) {
         <span style={{ color: '#FFBC17' }}> *</span>
         {props.mark ?? ''}
       </label>
-      <input className={styles.input} type={props.type} placeholder={props.description} />
+      <div className={styles.inputWrapper}>
+        <input className={styles.input} type={props.type} placeholder={props.description} />
+        {props.text === '이메일' && (
+          <button className={styles.button} onClick={props.onButtonClick}>
+            {props.buttonText ?? '확인'}
+          </button>
+        )}
+      </div>
     </div>
   );
 }
