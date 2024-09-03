8c99d1dd613db26a9d72d1004c4be93062d218bb
kwonboryong <kwonboryong@naver.com>
2024-09-03 16:34:50 +0900

    style: 입력칸 컴포넌트 디자인 수정

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,13 +4,11 @@ function Input(props) {
   return (
     <div className={styles.inputComponent}>
       <label className={styles.label} htmlFor="">
-        {props.text} {props.mark ?? ''}
+        {props.text}
+        <span style={{ color: '#FFBC17' }}> *</span>
+        {props.mark ?? ''}
       </label>
-      <input
-        className={styles.input}
-        type={props.type}
-        placeholder={props.description}
-      />
+      <input className={styles.input} type={props.type} placeholder={props.description} />
     </div>
   );
 }
