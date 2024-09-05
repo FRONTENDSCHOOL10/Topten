40628f16120ffc8ca7883e26eac727ca43344154
kwonboryong <kwonboryong@naver.com>
2024-09-05 15:50:37 +0900

    delete: 문서 제목 바꾸는 함수 제거 및 삭제 - react-helmet-async 사용으로 인해 따로 문서 제목 변경 함수를 사용하지 않아도 됨

--- src/pages/FindPasswordPage.jsx
+++ src/pages/FindPasswordPage.jsx
@@ -3,11 +3,11 @@ import Input from '@/components/Input/Input_kbr';
 import Button from '@/components/Button/Button_kbr';
 // import styles from '@/styles/pages/FindPasswordPage.module.scss';
 import styles from './../styles/pages/FindPasswordPage.module.scss';
-import useDocumentTitle from './../utils/useDocumentTitle';
 import { throttle } from './../utils/throttle';
 import Form from './../components/Form/Form';
 import pb from './../api/pocketbase';
 import toast, { Toaster } from 'react-hot-toast';
+import { Helmet } from 'react-helmet-async';
 
 // 유효성 검사 함수
 function validateName(name) {
@@ -21,8 +21,6 @@ function validateEmail(email) {
 }
 
 function FindPasswordPage(props) {
-  useDocumentTitle('비밀번호 찾기');
-
   // 이름 상태
   const [name, setName] = useState('');
 
@@ -140,6 +138,24 @@ function FindPasswordPage(props) {
 
   return (
     <>
+      <Helmet>
+        <title>비밀번호 찾기</title>
+        <meta
+          name="description"
+          content="Zustand를 사용하면 Context, useReducer, useState 없이 보다 효과적으로, 더 빠르게, 더 가볍게 상태를 관리할 수 있습니다."
+        />
+        <meta property="og:title" content="이듬(E.UID) 블렌디드 러닝" />
+        <meta property="twitter:title" content="이듬(E.UID) 블렌디드 러닝" />
+        <meta property="og:type" content="site" />
+        <meta property="og:url" content="https://yamoo9.github.io/EUID" />
+        <meta
+          property="og:description"
+          content="Front-End 개발자를 꿈꾸는 이들을 위한 블렌디드 러닝으로 개발에 필요한 모든 것!"
+        />
+        <meta property="og:image" content="https://yamoo9.github.io/EUID/og-image.jpg" />
+        <meta property="og:site:author" content="야무(yamoo9)" />
+      </Helmet>
+
       <div className={styles.findPassword}>
         <h2>비밀번호 찾기</h2>
         <p>
