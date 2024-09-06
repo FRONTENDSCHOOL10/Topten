5ab2eeada8163e3a1c6620e0153b40c888c9631b
kwonboryong <kwonboryong@naver.com>
2024-09-06 23:23:52 +0900

    update: Input 컴포넌트 타입 검사

--- src/components/Input/Input_kbr.jsx
+++ src/components/Input/Input_kbr.jsx
@@ -1,5 +1,25 @@
 import React, { useId } from 'react';
 import styles from './Input.module.scss';
+import PropTypes, { string, func, bool, oneOf, oneOfType, object, element } from 'prop-types';
+
+Input.propTypes = {
+  text: string.isRequired, // 버튼 텍스트
+  type: oneOf(['text', 'password', 'email', 'number']), // 입력 타입
+  description: string, // 입력칸의 placeholder 텍스트
+  value: string, // 입력칸의 값
+  name: string, // 입력칸의 name 속성 값
+  active: bool, // 버튼이 활성화된 상태인지 여부('이메일 인증 버튼')
+  inputRef: object, // 입력칸 참조
+  buttonText: string, // 버튼에 표시될 텍스트('이메일 인증 버튼')
+  onChange: func, // 입력 값이 변경될 때 호출
+  onBlur: func, // 입력칸에서 포커스가 벗어났을 때 호출
+  onButtonClick: func, // 버튼 클릭 시 호출
+  warningText: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool,
+  ]), // 경고 문구
+  warningStyle: object, // 경고 문구 스타일
+};
 
 function Input(props) {
   const id = useId();
