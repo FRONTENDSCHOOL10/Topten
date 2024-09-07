7d60c38fcd335903b95bfadb5e1fabcd96c82bf8
kwonboryong <kwonboryong@naver.com>
2024-09-07 19:12:33 +0900

    update: 룩북 컴포넌트 - 레이아웃 제작 - 버튼 컴포넌트 생성

--- src/components/Main/LookBook.jsx
+++ src/components/Main/LookBook.jsx
@@ -1,8 +1,29 @@
+import Button from './../Button/Button';
+import styles from './LookBook.module.scss';
+import pb from './../../api/getPbImageURL';
+
+// const item = {
+//   collectionId: "hkn8fmqpvlbc5rp",
+//   id: "hkn8fmqpvlbc5rp",
+//   photo: "image.jpg",
+// };
+
 function LookBook() {
+  // const imageUrl = getPbImageURL(item);
 
   return (
-    <div>
-      룩북
+    <div className={styles.container}>
+      <h2 className={styles.title}>Look Book : OOTD</h2>
+
+      <div>{/* <img src={imageUrl} alt="Sample" /> */}</div>
+
+      <Button
+        text="더 많은 룩북 보기"
+        backgroundColor="var(--primary-color)"
+        borderColor="var(--primary-color)"
+        fontWeight={400}
+        linkTo="/login"
+      />
     </div>
   );
 }
