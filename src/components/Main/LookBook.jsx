0daa9bf8fd7acd742259d5220ee39bc20ae7de08
kwonboryong <kwonboryong@naver.com>
2024-09-07 19:25:42 +0900

    update: '더 많은 룩북 보기' 클릭 시 룩북 페이지로 이동

--- src/components/Main/LookBook.jsx
+++ src/components/Main/LookBook.jsx
@@ -15,14 +15,23 @@ function LookBook() {
     <div className={styles.container}>
       <h2 className={styles.title}>Look Book : OOTD</h2>
 
-      <div>{/* <img src={imageUrl} alt="Sample" /> */}</div>
+      <div>
+        {/* 착용샷 */}
+        {/* <img src={imageUrl} alt="Sample" /> */}
+      </div>
+
+      <div>
+        <h3>관련 상품</h3>
+        {/* <img src={imageUrl} alt="Sample" /> */}
+        {/* <img src={imageUrl} alt="Sample" /> */}
+      </div>
 
       <Button
         text="더 많은 룩북 보기"
         backgroundColor="var(--primary-color)"
         borderColor="var(--primary-color)"
         fontWeight={400}
-        linkTo="/login"
+        linkTo="/lookbook"
       />
     </div>
   );
