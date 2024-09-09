e7d7ffe6da62f41acce3a310f664c621cf52e106
ksh17 <rkstkd98@naver.com>
2024-09-09 16:01:54 +0900

    feat: product 컴포넌트 기능 작성

--- src/components/Main/Product.jsx
+++ src/components/Main/Product.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import styles from './Product.module.scss';
 import initialCards from '@/data/test.js';
 import CostumeCard from '@/components/CostumeCard/CostumeCard';
+import Button from '../Button/Button';
+import { IoRefreshSharp } from 'react-icons/io5';
+
 const temperatureList = [
   '4° ↓',
   '5°~8°',
@@ -13,65 +16,12 @@ const temperatureList = [
   '28° ↑',
 ];
 
-//임시 카드 아이템
-const cards = [
-  {
-    id: 'aa1235',
-    costumeTitle: '키싱 시그널 오버핏 코튼',
-    costumeBrand: '무신사',
-    costumeLink: {
-      무신사: 'https://www.musinsa.com/products/4268814',
-    },
-    costumeImage: '/image/sc-favicon-512.png',
-    costumeSeason: ['봄'],
-    isRainsnow: true,
-    costumeTemperature: ['5°~8°'],
-    upperCategory: '상의',
-    lowerCategory: null,
-  },
-  {
-    id: 'aa12235',
-    costumeTitle: '봄 위',
-    costumeBrand: '무신사',
-    costumeLink: {
-      무신사: 'https://www.musinsa.com/products/4268814',
-    },
-    costumeImage: '/image/sc-favicon-512.png',
-    costumeSeason: ['봄'],
-    isRainsnow: true,
-    costumeTemperature: ['5°~8°'],
-    upperCategory: '상의',
-    lowerCategory: null,
-  },
-  {
-    id: 'aa12335',
-    costumeTitle: '겨울 바지',
-    costumeBrand: '무신사',
-    costumeLink: {
-      무신사: 'https://www.musinsa.com/products/4268814',
-    },
-    costumeImage: '/image/sc-favicon-512.png',
-    costumeSeason: ['겨울'],
-    isRainsnow: true,
-    costumeTemperature: ['5°~8°'],
-    upperCategory: 'null',
-    lowerCategory: '하의',
-  },
-  {
-    id: 'aa12365',
-    costumeTitle: '여름 가을 바지',
-    costumeBrand: '무신사',
-    costumeLink: {
-      무신사: 'https://www.musinsa.com/products/4268814',
-    },
-    costumeImage: '/image/sc-favicon-512.png',
-    costumeSeason: ['여름, 가을'],
-    isRainsnow: true,
-    costumeTemperature: ['5°~8°'],
-    upperCategory: 'null',
-    lowerCategory: '하의',
-  },
-];
+const BUTTONSTYLE = {
+  width: '66px',
+  height: '22px',
+  fontSize: '12px',
+  fontWeight: 400,
+};
 
 function Product() {
   const [likeList, setLikeList] = useState([]);
@@ -92,10 +42,13 @@ function Product() {
   // 랜더링 시 전체 프로덕트 아이템을 받아옴
   const [productItems, setProductItems] = useState(initialCards);
 
+  //다음 추천을 위한 index
+  const [nextIndex, setNextIndex] = useState(0);
+
   // 버튼 클릭시 해당 기온으로 temperature를 업데이트
   const handleClick = (e) => {
-    const { name } = e.target;
-    setTemperature({ current: name });
+    const { innerText } = e.target;
+    setTemperature({ current: innerText });
   };
 
   // 파생된 상태, 기온에 맞게 상의 하의를 필터링
@@ -104,24 +57,33 @@ function Product() {
       ({ costumeTemperature: t, upperCategory }) =>
         upperCategory === '상의' && t.includes(temperature.current)
     )
-    .slice(0, 2);
+    .slice(0 + nextIndex, 2 + nextIndex);
   const filteredLower = productItems
     .filter(
       ({ costumeTemperature: t, upperCategory }) =>
         upperCategory === '하의' && t.includes(temperature.current)
     )
-    .slice(0, 2);
+    .slice(0 + nextIndex, 2 + nextIndex);
+
+  // 새로고침 버튼 클릭 시 index를 증가시켜 다음 아이템으로 새로고침
+  const refreshProductItem = () => {
+    nextIndex === productItems.length - 2
+      ? setNextIndex((prev) => (prev = 0))
+      : setNextIndex((prev) => prev + 2);
+  };
 
   return (
-    <div>
-      <h2>오늘 날씨엔?</h2>
+    <div className={styles.product__component}>
+      <h2 className={styles.title}>오늘 날씨엔?</h2>
       <div className={styles.buttons}>
-        {temperatureList.map((temperature, index) => (
-          <Button key={index} value={temperature} onClick={handleClick} />
+        {temperatureList.map((text, index) => (
+          <Button style={BUTTONSTYLE} key={index} text={text} onClick={handleClick} />
         ))}
       </div>
-      <img src="/image/notification.png" alt="notification" />
-      <span className={styles.recommend}>반팔, 얇은 셔츠, 반바지, 면바지를 입으면 좋아요!</span>
+      <div className={styles.recommend__container}>
+        <img className={styles.icon} src="/image/notification.png" alt="notification" />
+        <span className={styles.recommend}>반팔, 얇은 셔츠, 반바지, 면바지를 입으면 좋아요!</span>
+      </div>
       <div className={styles.upper__section}>
         {filteredUpper.map((card) => (
           <CostumeCard
@@ -144,47 +106,15 @@ function Product() {
           />
         ))}
       </div>
+      <Button
+        style={{ marginTop: '30px' }}
+        text="다른 스타일 추천해드릴까요?"
+        onClick={refreshProductItem}
+        active="ture"
+        icon={<IoRefreshSharp />}
+      />
     </div>
   );
 }
 
 export default Product;
-
-// function Card({ imgPath, title, brand }) {
-//   return (
-//     <div>
-//       <div>
-//         <img className={styles.card} src={imgPath} alt="" />
-//         <p>{title}</p>
-//         <p>{brand}</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-function Button({ value, onClick }) {
-  return (
-    <button name={value} onClick={onClick}>
-      {value}
-    </button>
-  );
-}
-
-// {filteredUpper.map((item) => (
-//   <Card
-//     key={item.id}
-//     imgPath={item.costumeImage}
-//     title={item.costumeTitle}
-//     brand={item.costumeBrand}
-//   />
-// ))}
-// {filteredLower.map((item) => (
-//   <Card
-//     key={item.id}
-//     imgPath={item.costumeImage}
-//     title={item.costumeTitle}
-//     brand={item.costumeBrand}
-//   />
-// ))}
-
-// import initialCards from '@/data/test.js';
