f006a1b67b8ff064e52ba38791a373d07859635a
ksh17 <rkstkd98@naver.com>
2024-09-11 23:52:32 +0900

    feat: ChangeMyInfoPage 컴포넌트 생성 및 유저 닉네임 변경 기능 구현

---
+++ src/pages/ChangeMyInfoPage.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import EditHeader from '../components/EditHeader/EditHeader';
+import S from './../styles/pages/changePage.module.scss';
+import Input from '../components/Input/Input_kbr';
+import Button from '../components/Button/Button';
+import updateUserData from '../api/updateData';
+import loadToast from '../api/loadToast';
+import { Toaster } from 'react-hot-toast';
+
+const ChangeMyInfoPage = () => {
+  const [userNickName, setUserNickName] = useState('');
+  const user = JSON.parse(sessionStorage.getItem('pb_auth')).token;
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setUserNickName((prev) => (prev = value));
+  };
+
+  const handleClick = async () => {
+    try {
+      const updatedNickName = { ...user, userNickName };
+      const updatedUser = await updateUserData(user.id, updatedNickName);
+      loadToast('닉네임 변경 완료', '📌');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className={S.wrapComponent}>
+      <EditHeader
+        navText="회원정보 변경"
+        mainText="회원정보 변경"
+        description="닉네임 등 회원정보를 변경할 수 있어요"
+      />
+      <Input text="닉네임" onChange={handleChange} />
+      <Button text="회원정보 변경" onClick={handleClick} />
+      <Toaster />
+    </div>
+  );
+};
+
+export default ChangeMyInfoPage;
