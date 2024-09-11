370f577486e3304c7262e02531e9d166dee07139
kwonboryong <kwonboryong@naver.com>
2024-09-11 11:19:05 +0900

    update: 룩북 페이지 레이아웃 구성

--- src/pages/LookbookPage.jsx
+++ src/pages/LookbookPage.jsx
@@ -1,21 +1,27 @@
+import { useEffect } from 'react';
+import Button from './../components/Button/Button';
+import styles from './../styles/pages/LookbookPage.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+
 function LookbookPage(props) {
   return (
-    <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div>룩북</div>;
-    </>
+    <div className={styles.wrapComponent}>
+      <h2 className={styles.title}>Look Book : OOTD</h2>
+
+      <div className={styles.weatherIcon}>날씨</div>
+
+      <p className={styles.description}>
+        오늘 날씨엔 <br />
+        이런 스타일 어때요?
+      </p>
+
+      <div className={styles.outfitSwiper}>
+
+        <div className={styles.outfit}>착용샷</div>
+      </div>
+    </div>
   );
 }
 
