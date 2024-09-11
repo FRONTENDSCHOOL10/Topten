5d892be7438b71b0169730c3be0b27e4ba33b5a9
ksh17 <rkstkd98@naver.com>
2024-09-11 23:46:09 +0900

    feat: user get함수 추가

--- src/api/getData.js
+++ src/api/getData.js
@@ -3,3 +3,10 @@ import pb from './pocketbase';
 export async function getData(src, option) {
   return await pb.collection(src).getFullList(option);
 }
+
+export async function getUserInfo() {
+  pb.autoCancellation(false);
+  const uid = JSON.parse(sessionStorage.getItem('pb_auth')).token.id;
+  const data = await pb.collection('users').getOne(uid);
+  return data;
+}
