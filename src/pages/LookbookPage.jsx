9b2f809cfb758368c9696a633c1ffdcbbbc59f0f
kwonboryong <kwonboryong@naver.com>
2024-09-11 14:40:57 +0900

    update: 룩북 스와이퍼 네비게이션 버튼 외부로 빼기 & 아이콘 추가

--- src/pages/LookbookPage.jsx
+++ src/pages/LookbookPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Button from './../components/Button/Button';
 
 import pb from './../api/pocketbase';
@@ -6,16 +6,19 @@ import getPbImageURL from './../api/getPbImageURL';
 
 // 스와이퍼
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y, Keyboard } from 'swiper/modules';
+import { Pagination, Scrollbar, A11y, Keyboard } from 'swiper/modules';
 
 import 'swiper/scss';
-import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 
 import styles from './../styles/pages/LookbookPage.module.scss';
 
+// 아이콘
+import { GoChevronLeft } from 'react-icons/go';
+import { GoChevronRight } from 'react-icons/go';
+
 function LookbookPage(props) {
-  // 착용샷
+  // 착용샷 -----------------------
   const [lookBookItems, setLookBookItems] = useState([]);
 
   useEffect(() => {
@@ -39,31 +42,54 @@ function LookbookPage(props) {
     fetchLookBookItems();
   }, []);
 
+  // 착용샷 슬라이드 버튼 -----------------------
+  const swiperRef = useRef(null);
+
+  const goNext = () => {
+    swiperRef.current.swiper.slideNext();
+  };
+
+  const goPrev = () => {
+    swiperRef.current.swiper.slidePrev();
+  };
+
   return (
     <div className={styles.wrapComponent}>
       <h2 className={styles.title}>Look Book : OOTD</h2>
 
       <div className={styles.weatherIcon}>날씨</div>
 
-      <p className={styles.description}>
-        오늘 날씨엔 <br />
-        이런 스타일 어때요?
-      </p>
+      <div className={styles.subTitle}>
+        <p className={styles.description}>
+          오늘 날씨엔 <br />
+          이런 스타일 어때요?
+        </p>
+
+        <div className={styles.swiperBtn}>
+          <button className={styles.goPrev} type="button" onClick={goPrev}>
+            <GoChevronLeft />
+          </button>
+          <button className={styles.goNext} type="button" onClick={goNext}>
+            <GoChevronRight />
+          </button>
+        </div>
+      </div>
 
       <div className={styles.outfitSwiper}>
         <Swiper
           className={styles.swiper}
-          modules={[Navigation, Pagination, Scrollbar, A11y, Keyboard]}
+          modules={[Pagination, Scrollbar, A11y, Keyboard]}
           spaceBetween={20}
           slidesPerView={1.2}
+          loop={lookBookItems.length > 1}
           keyboard={{ enabled: true }}
-          navigation
           pagination={{ clickable: true }}
           a11y={{
             prevSlideMessage: '이전 슬라이드',
             nextSlideMessage: '다음 슬라이드',
             paginationBulletMessage: '페이지 {{index}}',
           }}
+          ref={swiperRef}
           // onSlideChange={() => console.log('slide change')}
           // onSwiper={(swiper) => console.log(swiper)}
         >
