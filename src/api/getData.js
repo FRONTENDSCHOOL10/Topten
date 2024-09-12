2220a14319f6ad7ae4201f725df2c412259ede57
ksh17 <rkstkd98@naver.com>
2024-09-12 14:30:49 +0900

    fix: 데이터에 isUser 추가

--- src/api/getData.js
+++ src/api/getData.js
@@ -6,7 +6,10 @@ export async function getData(src, option) {
 
 export async function getUserInfo() {
   pb.autoCancellation(false);
+  if (!sessionStorage.getItem('pb_auth')) {
+    return { isUser: false };
+  }
   const uid = JSON.parse(sessionStorage.getItem('pb_auth')).token.id;
   const data = await pb.collection('users').getOne(uid);
-  return data;
+  return { ...data, isUser: true };
 }
