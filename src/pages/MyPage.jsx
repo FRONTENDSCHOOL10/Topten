b3e4f36bf8558de20c5379d8fec2191ed2166e14
ksh17 <rkstkd98@naver.com>
2024-09-13 03:40:11 +0900

    feat: 아직 미완료

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,12 +5,36 @@ import useGetUserInfo from '../hooks/useGetUserInfo';
 import { NAV } from '../data/constant';
 import defaultImg from '/image/happiness.png';
 import { useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
+import { useState } from 'react';
+import updateUserData from '../api/updateData';
 
 function MyPage(props) {
   const navigate = useNavigate();
   const { user } = useGetUserInfo();
+  const [isActive, setIsActive] = useState(false);
   const profileImageUrl = user.isUser ? getPbImageURL(user, 'userPhoto') : defaultImg;
   const { userNickName, email, userSize: size, userColor } = user;
+  //console.log('user', user);
+  console.log(!user.userPhoto);
+  // const form = new FormData();
+  // form.append('user', user);
+  // console.log(form.get('user'));
+  // console.log(user);
+  const updateProfileImage = async (e) => {
+    // const [file] = e.target.files;
+    // const userPhoto = URL.createObjectURL(file);
+    // const form = new FormData();
+    // form.append('userPhoto', userPhoto);
+    // console.log(form.get('userPhoto').replace('blob:', ''));
+    // console.log({ ...user, userPhoto: form.get('userPhoto') }); // selectedFile must be File or Blob instance
+    // const aa = await updateUserData(user.id, {
+    //   ...user,
+    //   userPhoto: form.get('userPhoto').replace('blob:', ''),
+    // });
+    // console.log('aa', aa);
+    // console.log(user.id, { ...user, userPhoto: form.get('avatar') });
+  };
 
   //임시 모달
   const modal = !user.isUser && (
@@ -35,7 +59,7 @@ function MyPage(props) {
         </div>
         <div className={S.img__container}>
           <img className={S.profile__img} src={profileImageUrl} alt="프로필 이미지" />
-          <button className={S.camera__button}>
+          <button className={S.camera__button} onClick={() => setIsActive(true)}>
             <img className={S.camera__icon} src={'/icon/camera.png'} alt="프로필 이미지" />
           </button>
         </div>
@@ -45,8 +69,31 @@ function MyPage(props) {
           ? NAV.map(({ text, path }, index) => <NavList key={index} text={text} link={path} />)
           : ''}
       </ul>
+      <div className={clsx(S.profile__change__popup, { [S.active]: isActive })}>
+        <div>
+          <h2>사진 등록</h2>
+          <button onClick={() => setIsActive(false)}>
+            <img src="/icon/icon-button-close.png" />
+          </button>
+        </div>
+        <ul>
+          <li>
+            <img src="/icon/camera2.png" alt="" />
+            카메라로 촬영하기
+          </li>
+          <li>
+            <input
+              type="file"
+              accept="image/jpg,image/png,image/webp,image"
+              onChange={updateProfileImage}
+            />
+            <img src="/icon/picture.png" alt="" />
+            앨범에서 선택하기
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
-
+//className={clsx(S.button, { [S.active]: active })}
 export default MyPage;
