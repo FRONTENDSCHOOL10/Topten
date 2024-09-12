f4ddc21254dc7c9200920487e4b501e37b8b35ed
kwonboryong <kwonboryong@naver.com>
2024-09-12 12:08:02 +0900

    style: 룩북 상세 페이지 전체 레이아웃 구성

--- src/pages/LookBookDetailPage.jsx
+++ src/pages/LookBookDetailPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import LookBook from './../components/Main/LookBook';
 import pb from './../api/pocketbase';
+import styles from './../styles/pages/LookbookDetailPage.module.scss';
+import { GoChevronLeft } from 'react-icons/go';
 
 function LookBookDetailPage() {
   // 룩북 페이지에서 클릭된 착용샷
@@ -17,7 +19,6 @@ function LookBookDetailPage() {
           const item = await pb.collection('lookBook').getOne(itemId);
 
           setItem(item);
-          
         } catch (error) {
           console.error('아이템 상세 데이터를 가져오는 중 에러 발생:', error);
         }
@@ -27,7 +28,27 @@ function LookBookDetailPage() {
     fetchItemDetails();
   }, []);
 
-  return <div>{item ? <LookBook item={item} /> : <p>선택된 아이템이 없습니다.</p>}</div>;
+  return (
+    <div className={styles.wrapComponent}>
+      <div className={styles.topWrapper}>
+        <button className={styles.goPrev} type="button">
+          <GoChevronLeft />
+        </button>
+        <h2 className={styles.title}>Outfit of the Day </h2>
+      </div>
+
+      <div className={styles.weatherIcon}>날씨</div>
+
+      <div className={styles.subTitle}>
+        <p className={styles.description}>
+          저희가 추천하는 <br />
+          오늘의 OOTD 입니다.
+        </p>
+      </div>
+
+      <div>{item ? <LookBook item={item} /> : <p>선택된 아이템이 없습니다.</p>}</div>
+    </div>
+  );
 }
 
 export default LookBookDetailPage;
