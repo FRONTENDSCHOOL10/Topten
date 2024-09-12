9924e11f9e6a841a9acd4ca8dc7331e8a066831e
ksh17 <rkstkd98@naver.com>
2024-09-12 15:12:02 +0900

    fix: span 태그 button태그로 수정

--- src/components/NavList/NavList.jsx
+++ src/components/NavList/NavList.jsx
@@ -7,9 +7,9 @@ function NavList({ text, link }) {
   return (
     <li className={S.NavList}>
       {text}
-      <span onClick={() => navigate(link)}>
+      <button onClick={() => navigate(link)}>
         <img src={arrow} alt="페이지 이동 화살표" />
-      </span>
+      </button>
     </li>
   );
 }
