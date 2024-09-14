f2d4c6d999dda8dac750df72ecfc2000dc367c9e
ksh17 <rkstkd98@naver.com>
2024-09-14 22:50:08 +0900

    fix: 캘린더 임시로 예시 추가

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -1,5 +1,7 @@
 import CostumeCard from './../components/CostumeCard/CostumeCard';
-
+import { RiDeleteBin6Fill } from 'react-icons/ri';
+import { FaRegEdit } from 'react-icons/fa';
+import getPbImageURL from './../api/getPbImageURL';
 function CalendarPage(props) {
   //로컬에 저장한 북마크에서 입력한 데이터를 bookmarkData애 할당
   const bookmarkData = JSON.parse(localStorage.getItem('bookmarkItem'));
@@ -10,7 +12,15 @@ function CalendarPage(props) {
   return (
     <>
       <div>달력</div>
+      <br />
+      <br />
       <h1>임시</h1>
+      <button>
+        <RiDeleteBin6Fill />
+      </button>
+      <button>
+        <FaRegEdit />
+      </button>
       {bookmarkData && (
         <div>
           <h1>{bookmarkData.address}</h1>
@@ -18,10 +28,18 @@ function CalendarPage(props) {
           <p>{bookmarkData.comment}</p>
           <p>{bookmarkData.saveTime}</p>lowerItems
           {bookmarkData.upperItems.map((card) => (
-            <CostumeCard key={card.id} record={card} imageUrl={card.costumeImage} />
+            <CostumeCard
+              key={card.id}
+              record={card}
+              imageUrl={getPbImageURL(card, 'costumeImage')}
+            />
           ))}
           {bookmarkData.lowerItems.map((card) => (
-            <CostumeCard key={card.id} record={card} imageUrl={card.costumeImage} />
+            <CostumeCard
+              key={card.id}
+              record={card}
+              imageUrl={getPbImageURL(card, 'costumeImage')}
+            />
           ))}
         </div>
       )}
