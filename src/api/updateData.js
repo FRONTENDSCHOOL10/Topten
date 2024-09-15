b7c2add7eb6261da20d248975dac0bc7d21f6a30
ksh17 <rkstkd98@naver.com>
2024-09-15 19:08:49 +0900

    fix: update함수 수정

--- src/api/updateData.js
+++ src/api/updateData.js
@@ -1,7 +1,7 @@
 import pb from './pocketbase';
 
-export default async function updateUserData(id, data) {
-  const record = await pb.collection('users').update(id, data);
+export default async function updateUserData(collectionName, id, data) {
+  const record = await pb.collection(collectionName).update(id, data);
   return record;
 }
 
