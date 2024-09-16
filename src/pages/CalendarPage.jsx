34a2aba57da916950ecc1e2de000eda81eedb8ed
kwonboryong <kwonboryong@naver.com>
2024-09-16 16:26:29 +0900

    update: 캘린더 페이지 머지

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -6,6 +6,52 @@ import pb from '@/api/pocketbase';
 import { FaBookmark } from 'react-icons/fa';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 import 'react-calendar/dist/Calendar.css';
+import styled from 'styled-components';
+
+const StyledCalendar = styled(Calendar)`
+  border: none;
+
+  .react-calendar {
+    width: 450px;
+    max-width: 100%;
+    background: #ffffff;
+    font-family: 'Arial', sans-serif;
+    line-height: 1.5em;
+  }
+
+  /* 월 제목 */
+  .react-calendar__navigation__label {
+    font-family: Pretendard;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: normal;
+  }
+
+  /* 날짜 */
+  .react-calendar__month-view__days__day-names,
+  .react-calendar__month-view__days__day {
+    font-family: Pretendard;
+    font-size: 12px;
+    font-weight: 300;
+  }
+
+  /* 강조 표시된 날짜 스타일 */
+  .react-calendar__tile.markedDate {
+    background: #ffeb3b; /* 강조 표시 색상 */
+    color: #000; /* 글자 색상 */
+  }
+
+  // /* 북마크 표시 */
+  // .savedTimeLabel {
+  //   background-color: #ffbc17;
+  //   color: white;
+  //   font-size: 10px;
+  //   border-radius: 4px;
+  //   padding: 2px 4px;
+  //   position: absolute;
+  //   top: -10px;
+  // }
+`;
 
 const CalendarPage = (props) => {
   const [date, setDate] = useState(new Date());
@@ -107,7 +153,53 @@ const CalendarPage = (props) => {
   return (
     <>
       <div className="wrapComponent">
-        <Bookmark />
+        <div className={S.title}>
+          <p>원하시는 날짜를 선택하세요</p>
+          <button>달력 접기</button>
+        </div>
+
+        <div className={S.calendar}>
+          <StyledCalendar
+            className={S.custom__calender}
+            onChange={onChange}
+            value={date}
+            next2Label={null}
+            prev2Label={null}
+            showNeighboringMonth={false}
+            formatDay={(locale, date) => date.toLocaleString('en', { day: 'numeric' })}
+            tileContent={renderTileContent}
+            tileClassName={S.calender__tile}
+            onClickDay={handleDayClick}
+          />
+        </div>
+        
+        {visible && (
+          <div className={S.statusBar}>
+            <button type="button" className={S.arrow} onClick={() => goToBookmark('prev')}>
+              <IoIosArrowBack />
+            </button>
+            <p>
+              {/* 현재 북마크 날짜가 undefined가 아닌지 확인 */}
+              {bookmarkList[currentBookmarkIndex]?.date.slice(0, 12)
+                ? `${bookmarkList[currentBookmarkIndex].date.slice(0, 12)}`
+                : '북마크가 없습니다.'}
+            </p>
+            <button type="button" className={S.arrow} onClick={() => goToBookmark('next')}>
+              <IoIosArrowForward />
+            </button>
+          </div>
+        )}
+
+        <div className={S.BookmarkWrapper}>
+          {visible && (
+            <Bookmark
+              bookmark={bookmarkList[currentBookmarkIndex]}
+              bookmarkList={bookmarkList}
+              setBookmarkList={setBookmarkList}
+              currentBookmarkIndex={currentBookmarkIndex}
+            />
+          )}
+        </div>
       </div>
     </>
   );
