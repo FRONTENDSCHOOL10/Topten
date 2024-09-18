9c045b75b45dda186edcae12229866644e688267
EraMorgett4 <hsw1568@naver.com>
2024-09-18 20:52:37 +0900

    refactor: auth관련 zustand 상태 사용하도록 마이페이지 리팩토링 - NavList 컴포넌트의 index 추가 - NavList 구조 수정   - 해당 NavList 클릭 시 동작하도록 수정   - constant에 있던 NAV 자료구조 수정 - zustand 상태에 맞도록 마이페이지 구조 수정

--- src/components/NavList/NavList.jsx
+++ src/components/NavList/NavList.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import arrow from '/icon/right.svg';
 import S from './NavList.module.scss';
-function NavList({ text, link }) {
+function NavList({ text, link, onClick }) {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(); // onClick 이벤트가 있을 경우 실행
+    } else {
+      navigate(link); // onClick이 없으면 기본 링크로 이동
+    }
+  };
+
   return (
-    <li className={S.NavList} onClick={() => navigate(link)}>
+    <li className={S.NavList} onClick={handleClick}>
       {text}
       <button>
         <img src={arrow} alt="페이지 이동 화살표" />
