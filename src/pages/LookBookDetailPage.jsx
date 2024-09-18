1c958a15bbd1fee1329b8d8ca622b7839dc294dc
kwonboryong <kwonboryong@naver.com>
2024-09-18 23:04:34 +0900

    fix: 날씨 아이콘 불러오기 수정

--- src/pages/LookBookDetailPage.jsx
+++ src/pages/LookBookDetailPage.jsx
@@ -12,8 +12,10 @@ function LookBookDetailPage() {
   const { id } = useParams();
 
   // 날씨 아이콘
-  const skyCondition = localStorage.getItem('skyCondition');
-
+  const weatherData = JSON.parse(localStorage.getItem('weatherData'));
+  const skyCondition = weatherData ? weatherData.skyCondition : '';
+  const weatherIcon = getWeatherIcon(skyCondition);
+  
   // 룩북 페이지에서 클릭된 착용샷
   const [item, setItem] = useState(null);
 
@@ -65,7 +67,10 @@ function LookBookDetailPage() {
         </div>
 
         <div className={styles.weatherIcon}>
-          <img src={getWeatherIcon(skyCondition).src} alt={getWeatherIcon(skyCondition).alt} />
+        <img
+            src={weatherIcon.src}
+            alt={weatherIcon.alt}
+          />
         </div>
 
         <div className={styles.subTitle}>
