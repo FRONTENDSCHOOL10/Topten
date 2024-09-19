1a3af4aba93235e8f66b907dccebbb9f46cd0874
ksh17 <rkstkd98@naver.com>
2024-09-19 13:26:02 +0900

    fix: 주석 제거

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -3,9 +3,8 @@ import { getUserInfo } from '../api/getData';
 
 export default function useGetUserInfo() {
   const [user, setUser] = useState({});
-  console.log('sss');
+
   useEffect(() => {
-    console.log('aaaaaaaaaaaa');
     getUserInfo().then((userInfo) => setUser(userInfo));
   }, []);
   return { user, setUser };
