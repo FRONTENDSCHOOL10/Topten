0fa080920648ce646677e0a1efd5da6555890086
ksh17 <rkstkd98@naver.com>
2024-09-19 13:24:54 +0900

    feat: 온도에 맞는 추천 문구 리턴 함수 생성

--- src/data/constant.js
+++ src/data/constant.js
@@ -66,6 +66,42 @@ export function getInitTemperature(currentTemperature) {
   if (value >= 28) return '28° ↑';
 }
 
+export const outfitCategories = [
+  {
+    계절: '봄',
+    recommend: {
+      '20°~22°': '블라우스, 긴팔 티, 면바지, 슬랙스',
+    },
+  },
+  {
+    계절: '여름',
+    recommend: {
+      '23°~27°': '반팔, 얇은 셔츠, 반바지, 면바지',
+      '28° ↑': '민소매, 반팔, 반바지, 짧은 치마, 린넨 옷',
+    },
+  },
+  {
+    계절: '가을',
+    recommend: {
+      '12°~16°': '자켓, 가디건, 청자켓, 니트, 스타킹, 청바지',
+      '17°~19°': '얇은 가디건, 니트, 맨투맨, 후드, 긴 바지',
+    },
+  },
+  {
+    계절: '겨울',
+    recommend: {
+      '4° ↓': '패딩, 두꺼운 코트, 누빔 옷, 기모, 목도리',
+      '5°~8°': '울 코트, 히트텍, 가죽 옷, 기모',
+      '9°~11°': '트렌치 코트, 야상, 점퍼, 스타킹, 기모바지',
+    },
+  },
+];
+
+export function getRecommend(currentTemperature) {
+  const recommendObject = outfitCategories.filter((item) => item.recommend[currentTemperature]);
+  return recommendObject[0].recommend[currentTemperature];
+}
+
 export const bookmarkData = [
   [
     {
