43b12e797574758f983aa5dd460c89105e4d1fb8
ksh17 <rkstkd98@naver.com>
2024-09-19 23:57:38 +0900

    fix: 토스트 임시 제거

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -5,7 +5,7 @@ import { Toaster } from 'react-hot-toast';
 
 import pb from '../api/pocketbase';
 import updateUserData from '../api/updateData';
-import loadToast from '../api/loadToast';
+// import loadToast from '../api/loadToast';
 
 import { NAV } from '../data/constant';
 
@@ -118,8 +118,12 @@ const MyPage = () => {
           <div className={S.profile__info}>
             <h2>{user?.userNickName || '환영해요'}</h2>
             <p className={S.email}>{user?.email || 'E-mail'}</p>
-            <p className={S.size}>Size: <b>{user?.userSize?.join(', ') || ''}</b></p>
-            <p className={S.personal__color}>Personal color: <b>{user?.userColor?.join(', ') || ''}</b></p>
+            <p className={S.size}>
+              Size: <b>{user?.userSize?.join(', ') || ''}</b>
+            </p>
+            <p className={S.personal__color}>
+              Personal color: <b>{user?.userColor?.join(', ') || ''}</b>
+            </p>
           </div>
           <div className={S.img__container}>
             <div>
