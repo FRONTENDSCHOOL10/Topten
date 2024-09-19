8fe38911a9a4c76ac4b5b298334391959ef55f4a
ksh17 <rkstkd98@naver.com>
2024-09-19 23:38:56 +0900

    fix: 유저 정보 업데이트 수정

--- src/api/updateData.js
+++ src/api/updateData.js
@@ -1,21 +1,8 @@
 import pb from './pocketbase';
 
 export default async function updateUserData(collectionName, id, data) {
+  sessionStorage.setItem('pb_auth', JSON.stringify({ model: 'model', token: data }));
+  localStorage.setItem('pb_auth', JSON.stringify({ model: 'model', token: data }));
   const record = await pb.collection(collectionName).update(id, data);
   return record;
 }
-
-// const record = await pb.collection('users').update('RECORD_ID', data);
-// example update data
-// const data = {
-//   username: 'test_username_update',
-//   emailVisibility: false,
-//   password: '87654321',
-//   passwordConfirm: '87654321',
-//   oldPassword: '12345678',
-//   userID: 'test',
-//   userNickName: 'test',
-//   userGender: '남자',
-//   userSize: ['XS'],
-//   userColor: ['봄웜'],
-// };
