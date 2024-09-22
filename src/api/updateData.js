8b594e3d03c43baaf3b93e332fd05c33fb4082fa
ksh17 <rkstkd98@naver.com>
2024-09-22 21:28:54 +0900

    fix: 북마크 저장 시 pbAuth 훼손 수정

--- src/api/updateData.js
+++ src/api/updateData.js
@@ -1,8 +1,11 @@
 import pb from './pocketbase';
 
 export default async function updateUserData(collectionName, id, data) {
-  sessionStorage.setItem('pb_auth', JSON.stringify({ model: 'model', token: data }));
-  localStorage.setItem('pb_auth', JSON.stringify({ model: 'model', token: data }));
+  if (collectionName !== 'bookmarkItem') {
+    sessionStorage.setItem('pb_auth', JSON.stringify({ model: 'model', token: data }));
+    localStorage.setItem('pb_auth', JSON.stringify({ model: 'model', token: data }));
+  }
+
   const record = await pb.collection(collectionName).update(id, data);
   return record;
 }
